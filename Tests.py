b0ba8edb81a3acef0134d161d4f2cf502180ae95
BryanJoya <bryan.joya.1@ens.etsmtl.ca>
2025-05-14 19:42:39 +0000

    first attemps ci-cd

--- Tests.py
+++
@@ -1,5 +0,0 @@
-# test_hello.py
-from hello import get_greeting
-
-def test_get_greeting():
-    assert get_greeting() == "Hello World"
---
+++ test_hello.py
@@ -0,0 +1,7 @@
+"""Fichier de test pour la fonction get_greeting() dans hello.py."""
+# test_hello.py
+from hello import get_greeting
+
+def test_get_greeting():
+    """Teste si la fonction get_greeting retourne 'Hello World'."""
+    assert get_greeting() == "Hello World"
